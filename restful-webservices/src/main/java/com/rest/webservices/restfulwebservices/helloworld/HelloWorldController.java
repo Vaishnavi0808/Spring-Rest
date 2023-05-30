31575fc34422af6a6a698a4f90d88dd8ebbb6f33
Vaishnavi Sarode <sarodevaishnavi3@gmail.com>
2023-05-30 23:08:36 +0530

    Add files via upload

---
+++ restful-webservices/src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	//@RequestMapping(method=RequestMethod.GET, path="/hello-world")
+	@GetMapping(path="/hello-world")
+	public String helloWorld()
+	{
+		return "Hello My name is Vaishnavi";
+	}
+	
+//	@GetMapping(path="/hello-world-bean")
+//	public HelloWorldBean helloWorldBean()
+//	{
+//		return new HelloWorldBean("Hello My name is Vaishnavi");
+//	}
+
+}
+
